256bcf504a63c00531a5777cb347d841c2f3f2fe
Jack Willis <jack@attac.us>
2014-07-22 00:03:48 -0500

    knob visible

--- lissajous.js
+++ lissajous.js
@@ -61,6 +61,53 @@ window.onload=function(){
 
   ////
 
+  
+  var createKnobCSS = function(knob, id) {
+    var $input     = $(knob.element),
+        $container = $('<div class="container '+ id + '">'),
+        $body      = $('<div class="ui-knob ui-shadow '+ id + '">'),
+        $indicator = $('<div class="ui-indicator '+ id + '">');
+
+    $container.append($body);
+    $container.append($indicator);
+
+    // $input.hide();
+    $container.insertBefore($input);
+    $container.append($input);
+
+    // center knob in container
+    $body.css({
+      "margin-top": -($body.outerHeight()/2),
+      "margin-left":-($body.outerWidth()/2)
+    });
+
+    setupKnob(knob, $container[0]);
+  }
+
+  var drawKnobCSS = function(knob, indicator) {
+    var $indicator = $(knob.element).siblings('.ui-indicator');
+    $indicator.css({
+      left: indicator.x - $indicator.outerWidth()/2,
+      top:  indicator.y - $indicator.outerHeight()/2
+    });
+
+    var rotateText = 'rotate('+(-indicator.angle)+'deg)';
+    $indicator.css({
+      'transform': rotateText,
+      '-webkit-transform': rotateText,
+      '-moz-transform': rotateText,
+      '-o-transform': rotateText
+    });
+  }
+
+
+  var positionRotateKnob = new Knob(document.getElementById('position-rotate'),
+    function(knob, indicator) {
+      drawKnobCSS(knob, indicator);
+    }
+  );
+  createKnobCSS(positionRotateKnob, 'position-rotate');
+
 };
 
 function drawGrid() {
